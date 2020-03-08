ebacee2069a17da408680399fa51bb82b16b45b2
Nathan Bangwa <nathanbangwa7@gmail.com>
2020-03-08 05:15:55 +0000

    stable version of app without node js server

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import socket
 import json
 from random import randint
 from inference import Network
-### TODO: Import any libraries for MQTT and FFmpeg
+#Import any libraries for MQTT and FFmpeg
 import paho.mqtt.client as mqtt
 import sys
 
@@ -16,16 +16,23 @@ CPU_EXTENSION = os.path.join(os.getcwd(), "intel/openvino/deployment_tools/infer
 PVB_MODEL = os.path.join(os.getcwd(), "models/person-vehicle-bike-detection-crossroad-0078.xml")
 
 
-CLASSES = ["person", "vehicule", "bike"]
+CLASSES = ["person", "bike", "vehicule"]
 
 TotalObjtectCounter = [0] * 3
 
 # MQTT server environment variables
-HOSTNAME = socket.gethostname()
-IPADDRESS = socket.gethostbyname(HOSTNAME)
-MQTT_HOST = IPADDRESS
-MQTT_PORT = 3001  ### TODO: Set the Port for MQTT
+HOSTNAME = None
+IPADDRESS = None
+MQTT_HOST = None
+MQTT_PORT = 3001  #Set the Port for MQTT
 MQTT_KEEPALIVE_INTERVAL = 60
+
+def setServerEnvVariables():
+    global HOSTNAME, IPADDRESS, MQTT_HOST
+
+    HOSTNAME = socket.gethostname()
+    IPADDRESS = socket.gethostbyname(HOSTNAME)
+    MQTT_HOST = IPADDRESS
 
 def get_args():
     '''
@@ -54,17 +61,13 @@ def addStatistics(frame, classIDs):
     TotalObjtectCounter[1] += objectCounter[1]
     TotalObjtectCounter[2] += objectCounter[2]
 
-
     x = 50
 
     details = [
                 ["Current Persons detected: ", objectCounter[0], (x, 50)],
-                ["Current Vehicules detected: ", objectCounter[1], (x, 100)],
-                ["Current Bikes detected: ", objectCounter[2], (x, 150)],
-
-                ["Current Persons detected: ", TotalObjtectCounter[0], (x, 250)],
-                ["Current Vehicules detected: ", TotalObjtectCounter[1], (x, 300)],
-                ["Current Bikes detected: ", TotalObjtectCounter[2], (x, 350)]
+                ["Current Bikes detected: ", objectCounter[1], (x, 150)],
+                ["Current Vehicules detected: ", objectCounter[2], (x, 100)]
+                
 
             ]
     new_frame = None
@@ -72,7 +75,7 @@ def addStatistics(frame, classIDs):
     for text, nbObject, axis in details: 
         text += str(nbObject)
 
-        new_frame = cv2.putText(frame,text, axis, cv2.CV_FONT_HERSHEY_SIMPLEX, 2, 255)
+        new_frame = cv2.putText(frame,text, axis, cv2.FONT_HERSHEY_SIMPLEX, 2, 0)
     
     return new_frame, objectCounter
 
@@ -93,23 +96,7 @@ def draw_boxes(frame, result, args, width, height):
             ymax = int(box[6] * height)
             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 0, 255), 1)
 
-    return frame
-
-def draw_masks(result, width, height):
-    '''
-    Draw semantic mask classes onto the frame.
-    '''
-    # Create a mask with color by class
-    classes = cv2.resize(result[0].transpose((1,2,0)), (width,height), 
-        interpolation=cv2.INTER_NEAREST)
-    unique_classes = np.unique(classes)
-    out_mask = classes * (255/20)
-    
-    # Stack the mask so FFmpeg understands it
-    out_mask = np.dstack((out_mask, out_mask, out_mask))
-    out_mask = np.uint8(out_mask)
-
-    return out_mask, unique_classes
+    return frame, classes
 
 
 def get_class_names(class_nums):
@@ -120,10 +107,13 @@ def get_class_names(class_nums):
 
 
 def infer_on_video(args, model):
-    ### TODO: Connect to the MQTT server
-    client = mqtt.Client()
-        
-    client.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+    if args.p == "YES":
+        setServerEnvVariables
+
+        #Connect to the MQTT server
+        client = mqtt.Client()
+            
+        client.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
 
     # Initialize the Inference Engine
     plugin = Network()
@@ -167,20 +157,32 @@ def infer_on_video(args, model):
             out_frame, classes = draw_boxes(frame, result, args, width, height)
             class_names = get_class_names(classes)
 
-            out_frame_local, stats = addStatistics(out_frame)
+            out_frame_local, stats = addStatistics(out_frame, classes)
 
             # write out the frame
             out.write(out_frame_local)
             
-            ### TODO: Send the class names and speed to the MQTT server
-            ### Hint: The UI web server will check for a "class" and
-            ### "speedometer" topic. Additionally, it expects "class_names"
-            ### and "speed" as the json keys of the data, respectively.
+            #Send the class names and speed to the MQTT server
             # publish class
             if args.p == "YES":
+                # person counter
+                personCount = {
+                    "personCount": stats[0]
+                }
+
+                # bike counter
+                personCount = {
+                    "bikeCount": stats[1]
+                }
+
+                # vehicule counter
+                personCount = {
+                    "vehiculeCount": stats[2]
+                }
+
                 speed = randint(50,70)
                 dataClass = {
-                    "class_names": class_names,
+                    "class_names": "class_names",
                 }
                 dataClass = json.dumps(dataClass)
                 
@@ -196,7 +198,7 @@ def infer_on_video(args, model):
                 client.publish("speedometer", dataSpeed)
         
         if args.p == "YES":
-            ### TODO: Send frame to the ffmpeg server
+            #Send frame to the ffmpeg server
             sys.stdout.buffer.write(out_frame)  
             sys.stdout.flush()
         
@@ -209,8 +211,10 @@ def infer_on_video(args, model):
     # Release the capture and destroy any OpenCV windows
     cap.release()
     cv2.destroyAllWindows()
-    ### TODO: Disconnect from MQTT
-    client.disconnect()
+
+    if args.p == "YES":
+        #Disconnect from MQTT
+        client.disconnect()
 
 
 def main():
